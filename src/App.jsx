5e7d186046ee0bec9da371fe61029bb3c5cf069a
azotamiota <azotamiota@gmail.com>
2022-08-23 10:58:54 +0100

    Here it comes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
-import React from "react"; 
+import React, {useState, useEffect} from "react"; 
+import axios from "axios";
+
+import ShowCard from './components/ShowCard'
+
+import './App.css'
+
+// const [showData, setShowData] = useState([{title: "Afterlife", description: "Lorem ipsum lorem ipsum", rating: 9.1},{title: "Black Mirror", description: "Lorem ipsum lorem ipsum", rating: 9.8},{title: "Walking dead", description: "Lorem ipsum lorem ipsum", rating: 8.2}])
 
 const App = () => {
+    
+    const [showData, setShowData] = useState([]);
 
-    return <h1>Hello!!!!</h1>
+    useEffect(() => {
+
+        const searchAPi = async () => {
+            const result = await axios.get('https://api.tvmaze.com/search/shows?q=girls')
+            setShowData(result.data)
+        }
+
+        searchAPi();
+
+    }, [])
+
+    // https://api.tvmaze.com/search/shows?q=girls
+
+
+    return <>
+        <form id='search-form'></form>
+        {showData.map(show => <ShowCard key={show.show.id} data={show.show}/>)}
+    </>
 
 }
 
---
+++ src/components/ShowCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ShowCard = ({data}) => {
+
+   return <div className="show-card">
+            <h2>{data.name}</h2>
+            {/* <p>{description}</p>
+            <div>{rating}</div> */}
+        </div>
+}
+
+export default ShowCard;
