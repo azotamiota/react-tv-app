1a925f459e6964b200bfc57922e2170c16109cd6
azotamiota <azotamiota@gmail.com>
2022-08-23 11:54:44 +0100

    add images

--- src/components/ShowCard/index.jsx
+++ src/components/ShowCard/index.jsx
@@ -4,8 +4,9 @@ const ShowCard = ({data}) => {
 
    return <div className="show-card">
             <h2>{data.name}</h2>
-            {/* <p>{description}</p>
-            <div>{rating}</div> */}
+            <img src={data['image'] ? data['image']['medium'] : ''}/>
+            <p>{data.summary}</p>
+            {/* <div>{rating}</div> */}
         </div>
 }
 
